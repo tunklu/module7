a70990f977cd55c04726201b81fc2a3cc0357e39
Denis <vin_ogr@list.ru>
2024-11-08 12:45:59 +0700

    module 7.2

---
+++ module_7_2.py
@@ -0,0 +1,16 @@
+def custom_write(file_name, strings):
+    file = open(file_name, 'a')
+    file.close()
+
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
