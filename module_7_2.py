86dfced91c614fa8d38566621c4c1af1e18a8fa2
Denis <vin_ogr@list.ru>
2024-11-08 13:33:38 +0700

    module 7.2

--- module_7_2.py
+++ module_7_2.py
@@ -1,6 +1,13 @@
 def custom_write(file_name, strings):
-    file = open(file_name, 'a')
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_position = {}
+    index_string = 1
+    for text_line in strings:
+        strings_position[index_string, file.tell()] = text_line
+        file.write(text_line + '\n')
+        index_string += 1
     file.close()
+    return strings_position
 
 
 
