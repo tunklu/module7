0d2cd9730fcf4f27be917f5a0a5418786b1ee6bf
Denis <vin_ogr@list.ru>
2024-11-09 21:00:14 +0700

    module_7_3

--- module_7_3.py
+++ module_7_3.py
@@ -1,6 +1,5 @@
-
+import string
 from pprint import pprint
-
 
 class WordsFinder:
     file_names = []
@@ -8,16 +7,36 @@ class WordsFinder:
         self.file_names = args
 
     def get_all_words(self):
-        all_worlds = dict()
-        for one_file_name in self.file_names:
-            open_file = open(one_file_name, 'r', encoding='utf-8')
-            key = one_file_name
-            for world in open_file:
-                value = world.split()
-                all_worlds[key] = value
-                print(value)
-            open_file.close()
-        return all_worlds
+        all_words = {}
+        for file_name in self.file_names:
+           with (open(file_name, 'r', encoding='utf-8') as open_file):
+                words_content = open_file.read().lower()
 
-finder2 = WordsFinder('test_file.txt')
+                str_punk = str.maketrans(',', ',', string.punctuation + '-')
+                words_content = words_content.translate(str_punk)
+                words = words_content.split()
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        word = word.lower()
+        for file_name, words in all_words.items():
+                if word in words:
+                    result[file_name] = words.index(word)+1
+                return word, result
+
+
+    def count(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        word = word.lower()
+        for file_name, words in all_words.items():
+            result[file_name] = words.count(word)
+            return result
+
+finder2 = WordsFinder('test_file.txt', 'test.txt')
 print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
